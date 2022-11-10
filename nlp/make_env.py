3ba2db1cf1ab0554a2b495bd8702fcdc4a1f5db5
Israel Sánchez Miranda <58601226+tamagochi-descompuesto@users.noreply.github.com>
2022-11-09 19:10:27 -0600

    Update make_env.py

--- nlp/make_env.py
+++ nlp/make_env.py
@@ -2,5 +2,5 @@ with open('.env', 'w') as file:
     file.write('REDUCED_NER_DATASET=True')
     file.write('NER_EPOCHS=10')
     file.write('X_RAPID_API_KEY=YOUR_API_KEY')
-    file.write('X_RAPID_API_HOST=YOUR_API_HOST_GOOGLE')
+    file.write('X_RAPID_API_HOST_DEEP=YOUR_API_HOST')
 file.close()
